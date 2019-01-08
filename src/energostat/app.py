a4f0d5ef6839624c1ffc210dfb640ff615eb44de
b7w@isudo.ru <b7w@isudo.ru>
2019-01-08 03:13:35 +0300

    Add flask

--- src/energostat/app.py
+++ src/energostat/app.py
@@ -1,10 +1,11 @@
 import logging
+from io import BytesIO
 from itertools import groupby
 
 from energostat.input import read_zip, read_html
 from energostat.model import key_date, key_sensor
-from energostat.utils import fix_data
 from energostat.output import create_xml, create_zip
+from energostat.utils import fix_data
 
 logger = logging.getLogger('root')
 
@@ -28,3 +29,15 @@ def html2xml(f_in, f_out):
         logger.debug('XML: %s', msg)
         messages.append(msg)
     create_zip(f_out, messages)
+
+
+def html2xml_bytes(file):
+    f_in = BytesIO()
+    f_out = BytesIO()
+
+    f_in.write(file.read())
+    f_in.seek(0)
+
+    html2xml(f_in, f_out)
+    f_out.seek(0)
+    return f_out
--- src/main.py
+++ src/main.py
@@ -3,13 +3,15 @@
 import logging
 from logging.config import dictConfig
 
-from energostat.app import html2xml
+from flask import Flask, send_from_directory, request, redirect, send_file
+
+from energostat.app import html2xml_bytes
 
 logger = logging.getLogger('root')
 
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
+    'disable_existing_loggers': True,
     'formatters': {
         'simple': {
             'format': '%(levelname)-5s [%(asctime)s, %(msecs)d] %(name)s.%(funcName)s at %(lineno)d: %(message)s',
@@ -35,14 +37,38 @@ LOGGING = {
     },
 }
 
+app = Flask(__name__)
 
-def main():
-    dictConfig(LOGGING)
-    logger.info('Starting app')
 
-    with open('tmp/sensors.zip', 'rb') as f_in, open('tmp/messages.zip', 'wb') as f_out:
-        html2xml(f_in, f_out)
+@app.route('/')
+def index():
+    return send_from_directory('static', 'index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory('static', 'favicon.ico')
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    if 'file' not in request.files:
+        logger.warning('No file part')
+        return redirect('')
+    file = request.files['file']
+    if file.filename == '':
+        logger.warning('No selected file')
+        return redirect('')
+    if file:
+        f_out = html2xml_bytes(file)
+        return send_file(
+            f_out,
+            attachment_filename='messages.zip',
+            mimetype='application/zip'
+        )
+    return redirect('')
 
 
 if __name__ == '__main__':
-    main()
+    dictConfig(LOGGING)
+    app.run()
